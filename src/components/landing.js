bf010993757261b45e69193ba0c2c0b8dbd874c8
Haji Mohammed <hajisy07@gmail.com>
2020-10-07 22:25:44 +0100

    fixed typo

--- src/components/landing.js
+++ src/components/landing.js
@@ -42,7 +42,7 @@ function Landing() {
     <div className="w-full">
       <div className="w-full py-48 md:py-24 md:flex md:items-center md:justify-between" ref={sectionTrigger}>
         <div className="md:w-1/2">
-          <h1 className="text-4xl lg:text-5xl font-bold tracking-wider">Hi, I'm Haji Mohammmed</h1>
+          <h1 className="text-4xl lg:text-5xl font-bold tracking-wider">Hi, I'm Haji Mohammed</h1>
           <p className="mt-10">Front End Developer, UI/UX Design</p>
         </div>
         <div className="hidden md:block" style={{ width: 400, height: 500 }}>
