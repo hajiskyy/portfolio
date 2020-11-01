bae1ba7230df1e5cbb5d6af3a08033e279806683
Haji Mohammed <hajisy07@gmail.com>
2020-11-01 18:03:02 +0100

    for vercel

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = ({ siteTitle }) => (
       <div><Link href="/" ><a className="text-xl font-bold tracking-widest">{siteTitle}</a></Link></div>
       <div className="w-full md:w-3/5 flex justify-between items-center mt-8 md:mt-0 text-gray-700 transition-all ease-out">
         <div><Link href="/#about"><a className="hover:text-black">About Me</a></Link></div>
-        <div><Link href="/#projects"><a className="hover:text-black">Projects</a></Link></div>
+        <div><Link href="/#projects"><a className="hover:text-black"> Projects</a></Link></div>
         <div><Link href="/#stack"><a className="hover:text-black">Stack</a></Link></div>
         <div><Link href="/#contact"><a className="hover:text-black">Contact</a></Link></div>
         <div><a href="https://github.com/hajiskyy" target="_blank" rel="noreferrer"><Github className="text-gray-900 icons" /></a></div>
